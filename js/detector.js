edcf9fff724e3498c204a21e102ac16abc6a85ea
Deep <sekharsmemories@gmail.com>
2023-12-19 14:16:37 +0530

    st

---
+++ js/detector.js
@@ -0,0 +1,7 @@
+console.log("Hello from content.js");
+document.body.style.backgroundColor = "red";
+// change document body color 
+// document.body.getElementsByClassName("nav-opt-sprite")[0].style.backgroundColor = "red";
+
+// print the document html code 
+// console.log(document.documentElement.innerHTML);
