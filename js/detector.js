b5c6da084972805361085bd004a510b44f392bea
Deep <sekharsmemories@gmail.com>
2024-01-24 00:22:46 +0530

    start

--- js/detector.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello from content.js");
-document.body.style.backgroundColor = "red";
-// change document body color 
-// document.body.getElementsByClassName("nav-opt-sprite")[0].style.backgroundColor = "red";
-
-// print the document html code 
-// console.log(document.documentElement.innerHTML);
